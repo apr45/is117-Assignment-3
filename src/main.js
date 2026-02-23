faeb8a31fff0b3aaf2c525359e8284dd5a3f191d
Alex <apr45@njit.edu>
2026-02-23 14:37:52 -0500

    small edits on main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // 1. Configuration: Path to the project markdown file
-const PROJECT_PATH = '../public/it286Project.md';
+const PROJECT_PATH = '../it286Project.md';
 
 /**
  * Simple Markdown to HTML Parser
